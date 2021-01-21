d6853e4350a930094775ae75f36423d9a58e0c1c
Yahya <yahyatinani@gmail.com>
2021-01-21 17:55:07 +0100

    Add a default splash screen

---
+++ android/src/main/java/com/why/template/compose/view/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.why.template.compose.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}

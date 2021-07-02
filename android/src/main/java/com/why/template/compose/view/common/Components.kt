dc8b915c90c7b061dee0b5d644c42a4f0d7a65fe
Yahya <yahyatinani@gmail.com>
2021-07-02 21:18:42 +0100

    Add a background image to the app component

--- android/src/main/java/com/why/template/compose/view/common/Components.kt
+++ android/src/main/java/com/why/template/compose/view/common/Components.kt
@@ -1,10 +1,17 @@
 package com.why.template.compose.view.common
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.why.template.compose.R
 import com.why.template.compose.theme.MyTheme
 import com.why.template.compose.view.home.topAppBar
 
@@ -13,7 +20,17 @@ fun MyApp(topAppBarText: String = "Title", content: @Composable () -> Unit) {
     MyTheme {
         Scaffold(topBar = topAppBar(topAppBarText)) {
             Surface {
-                content()
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        modifier = Modifier.align(Alignment.Center),
+                        painter = painterResource(
+                            id = R.drawable.product_logo_compose_color_192
+                        ),
+                        contentDescription = "",
+                        alpha = 0.05f
+                    )
+                    content()
+                }
             }
         }
     }

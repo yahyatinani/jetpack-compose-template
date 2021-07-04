d84ca24cf32e193ec202421b645c97f938a4b6eb
Yahya <yahyatinani@gmail.com>
2021-07-04 13:16:39 +0100

    Upgrade Gradle plugin

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -10,7 +10,7 @@ object Plugins {
     }
 
     object Android {
-        private const val version = "7.0.0-beta04"
+        private const val version = "7.0.0-beta05"
 
         const val gradle = "com.android.tools.build:gradle:$version"
     }

277a1da392cd30ccbcc2667cfca806d3f095629a
Yahya <yahyatinani@gmail.com>
2021-07-15 14:29:26 +0100

    Delete dead code

--- android/src/main/java/com/why/template/compose/materialisedview/core.kt
+++ android/src/main/java/com/why/template/compose/materialisedview/core.kt
@@ -6,12 +6,3 @@ data class MainViewModel(
     val topBarTitle: String = "",
     val currentPage: Route = Route.NONE
 )
-
-fun pageViewModel(
-    viewModel: MainViewModel,
-    title: String,
-    route: Route
-): MainViewModel = viewModel.copy(
-    currentPage = route,
-    topBarTitle = title
-)

fd5e2bd272c8b81dcafb982f0184a1d89438e976
Yahya <yahyatinani@gmail.com>
2021-07-20 11:48:31 +0100

    [WIP] Implement dbHandlerToInterceptor, doFx

--- android/src/main/java/com/why/template/compose/recompose/core.kt
+++ android/src/main/java/com/why/template/compose/recompose/core.kt
@@ -3,6 +3,8 @@ package com.why.template.compose.recompose
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.why.template.compose.recompose.Context.*
+import com.why.template.compose.recompose.Interceptor.*
 import com.why.template.compose.recompose.db.MainViewModel
 import com.why.template.compose.recompose.db.appDb
 import com.why.template.compose.recompose.db.appState
@@ -19,6 +21,19 @@ val fxHandlers = ConcurrentHashMap<Any, Any>()
 val queryFns = ConcurrentHashMap<Any, Any>()
 val memSubComp = ConcurrentHashMap<Any, Any>()
 
+enum class Interceptor {
+    Id,
+    Before,
+    After
+}
+
+enum class Context {
+    Effects,
+    Coeffects,
+    Db,
+    Event
+}
+
 fun regEventFx(
     id: Any,
     handler: (cofx: Map<Any, Any>, vec: ArrayList<Any>) -> Map<Any, Any>
@@ -31,15 +46,106 @@ fun regEventFx(
 
 }
 
+/***
+ * Associate the given event `id` with the given collection of `interceptors`.
+ */
+fun register(id: Any, interceptors: ArrayList<Any>) {
+    TODO()
+}
+
+fun toInterceptor(
+    id: Any,
+    before: (context: Map<Any, Any>) -> Any = {},
+    after: (context: Map<Any, Any>) -> Any = {}
+): Map<Interceptor, Any> = mapOf(
+    Id to id,
+    Before to before,
+    After to after,
+)
+
+/**
+ * Context:
+ *
+ * {:coeffects {:event [:some-id :some-param]
+ *              :db    <original contents of app-db>}
+ *
+ *  :effects   {:db        <new value for app-db>
+ *              :dispatch  [:an-event-id :param1]}
+ *
+ *  :queue     [a collection of further interceptors]
+ *
+ *  :stack     [a collection of interceptors already walked]}
+ */
+fun dbHandlerToInterceptor(
+    handlerFn: (vm: MainViewModel, vec: ArrayList<Any>) -> MainViewModel
+): Map<Interceptor, Any> = toInterceptor(
+    id = ":db-handler",
+    before = { context: Map<Any, Any> ->
+        val cofx = context[Coeffects] as Map<*, *>
+        val db = cofx[Db] as MainViewModel
+        val event = cofx[Event] as ArrayList<Any>
+
+        val newDb = handlerFn(db, event)
+
+        val newContext = cofx.plus(Db to newDb).let { newCofx ->
+            context.plus(Coeffects to newCofx)
+        }
+
+        newContext
+    }
+)
+
+val doFx: Map<Interceptor, Any> = toInterceptor(
+    id = ":do-fx",
+    after = { context: Map<Any, Any> ->
+        val effects = context[Effects] as Map<*, *>
+        val effectsWithoutDb = effects.minus(Db)
+
+        val newDb = effects[Db] as MainViewModel?
+        if (newDb != null) {
+            val fxFn = fxHandlers[":db"] as (value: Any) -> Unit
+            fxFn(newDb)
+        }
+
+        for ((effectKey, effectValue) in effectsWithoutDb) {
+            val fxFn = fxHandlers[effectKey] as ((value: Any) -> Unit)?
+
+            if (fxFn != null && effectValue != null)
+                fxFn(effectValue)
+            else
+                Log.i(
+                    "re-compose",
+                    "no handler registered for effect: $effectKey. Ignoring."
+                )
+        }
+    }
+)
+
+/**
+ * Register the given event `handler` (function) for the given `id`.
+ */
+fun regEventDb(
+    id: Any,
+    interceptors: ArrayList<Any>,
+    handler: (vm: MainViewModel, vec: ArrayList<Any>) -> MainViewModel
+) {
+//    Log.i("regEventDb", "$id")
+//
+//    if (dbEvent[id] != null)
+//        Log.w("regEventDb: ", "overwriting handler for: $id")
+//    dbEvent[id] = handler
+
+    register(
+        id,
+        arrayListOf(doFx, interceptors, dbHandlerToInterceptor(handler))
+    )
+}
+
 fun regEventDb(
     id: Any,
     handler: (vm: MainViewModel, vec: ArrayList<Any>) -> MainViewModel
 ) {
-    Log.i("regEventDb", "$id")
-
-    if (dbEvent[id] != null)
-        Log.w("regEventDb: ", "overwriting handler for: $id")
-    dbEvent[id] = handler
+    regEventDb(id, arrayListOf(), handler)
 }
 
 fun regFx(id: Any, handler: (value: Any) -> Unit) {
@@ -131,10 +237,6 @@ class Framework : ViewModel() {
 
         Log.i("onCleared", "Resources released")
         halt()
-    }
-
-    companion object {
-
     }
 }
 

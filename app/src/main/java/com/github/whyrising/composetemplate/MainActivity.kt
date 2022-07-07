60dd2a99985d74fd34e3300a5106e583643c3565
Yahya <47183730+whyrising@users.noreply.github.com>
2022-07-07 14:49:25 +0100

    Add splshscreen support

--- app/src/main/java/com/github/whyrising/composetemplate/MainActivity.kt
+++ app/src/main/java/com/github/whyrising/composetemplate/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.github.whyrising.composetemplate.ui.theme.JetpackComposeTemplateTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
+    
     setContent {
       JetpackComposeTemplateTheme {
         // A surface container using the 'background' color from the theme

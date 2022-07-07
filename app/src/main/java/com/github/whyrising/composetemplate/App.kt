340c28b8c05178bc9aeef5c5828f0a71a4c9e52e
Yahya <47183730+whyrising@users.noreply.github.com>
2022-07-07 22:04:13 +0100

    Fix splashscreen not showing

---
+++ app/src/main/java/com/github/whyrising/composetemplate/App.kt
@@ -0,0 +1,11 @@
+package com.github.whyrising.composetemplate
+
+import android.app.Application
+
+class App : Application() {
+  override fun onCreate() {
+    super.onCreate()
+
+    // TODO: init
+  }
+}
--- app/src/main/java/com/github/whyrising/composetemplate/MainActivity.kt
+++ app/src/main/java/com/github/whyrising/composetemplate/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,17 +17,24 @@ import com.github.whyrising.composetemplate.ui.theme.TemplateTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    installSplashScreen()
+    installSplashScreen().apply {
+      // TODO: remove this.
+//       setKeepOnScreenCondition { true }
+    }
     super.onCreate(savedInstanceState)
 
     setContent {
       TemplateTheme {
         // A surface container using the 'background' color from the theme
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colors.background
-        ) {
-          Greeting("Android")
+        Scaffold { paddingValues ->
+          Surface(
+            modifier = Modifier
+              .fillMaxSize()
+              .padding(paddingValues),
+            color = MaterialTheme.colors.background
+          ) {
+            Greeting("Android")
+          }
         }
       }
     }
